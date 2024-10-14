6290d3bb841c4a0e88fe7e059726295f2b41d7fd
m4nn4t <144805614+m4nn4t@users.noreply.github.com>
2024-10-14 17:21:25 +0530

    first practice question

---
+++ linkedlist.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+
+class Node{
+    public:
+
+    int data;
+    Node* next;
+
+    public:
+
+
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+
+
+};
+
+
+Node* Converting_arr_to_LL(int arr[],int n){
+    Node* mover=new Node(arr[0]);
+    Node* head=mover;
+    for(int i=0;i<n;i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+    }
+
+    return head;
+
+}
+
+int main(){
+    int n=5;
+    int arr[n]={7,3,4,2,1};
+
+    Converting_arr_to_LL(arr,n);
+
+    return 0;
+
+}
+
+
